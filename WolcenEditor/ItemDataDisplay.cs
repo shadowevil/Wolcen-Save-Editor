adf4ddf5e55d7c252ae101da4f9a8df4b723683e
ShadowEvil <shadow3vil@yahoo.com>
2020-03-16 23:23:31 -0500

    First commit

--- WolcenEditor/ItemDataDisplay.cs
+++ WolcenEditor/ItemDataDisplay.cs
@@ -190,6 +190,11 @@ namespace WolcenEditor
                     if (s_Effect.Contains("%1") || s_Effect.Contains("%2"))
                     {
                         if (effect.EffectId.Contains("percent")) s_Effect = s_Effect.Replace("%1", "%1%");
+                        if (effect.Parameters.Count <= 0)
+                        {
+                            LogMe.WriteLog("Error: effectID(" + effect.EffectId + "), effectName(" + effect.EffectName + ") missing semantics!");
+                            return;
+                        }
                         s_Effect = s_Effect.Replace("%1", "+" + effect.Parameters[0].value.ToString());
                         if (s_Effect.Contains("%2")) s_Effect = s_Effect.Replace("%2", effect.Parameters[1].value.ToString());
                     }
